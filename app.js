4b2e5d7ab86b047ee8e4b8d79fdbe1c7a334efd8
Erik Plyushko <eplyushko@gmail.com>
2017-07-17 18:33:12 -0700

    Out of the woods/pike/done

--- app.js
+++ app.js
@@ -2,22 +2,42 @@
 var hrPd = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
 
 var pike = {
-  minCustPrHr: 23,
-  maxCustPrHr: 65,
-  avgCookiesPerCust: 6.3,
-  customersEachHour: [],
-  cookiesEachHour: [],
-  totalCookiesPerDay:0,
-  //(methods to calculate customer and cookie totals)
-  //(method to calculate customers per hour)
-  random: function(min,max){
-    return (Math.floor(Math.random()* (max - min + 1)) + min);
+  minCust: 23,
+  maxCust: 65,
+  avgCookies: 6.3,
+  cookiesPrHr:[],
+  custPh: function(minCust,maxCust){
+    return (Math.floor(Math.random()* (this.maxCust - this.minCust + 1)) + this.minCust);
   },
-  ckPrh: function(){
-    var rand = this.random(this.minCustPrHr,maxCustPrHr);
-    var prD = ((rand * avgCookiesPerCust) /15);
+  cookiesPerH: function(){
+    for(var i = 0; i < hrPd.length; i++){
+      var cookiesPer = Math.floor(this.avgCookies * this.custPh());
+      this.cookiesPrHr.push(cookiesPer);
+    }
+    return this.cookiesPrHr;
+  },
+  cookiesPerDay: function(){
+    var total = 0;
+    for(var i = 0; i < this.cookiesPrHr.length; i++){
+      total += this.cookiesPrHr[i];
+    }
+    return total;
+  },
+  render: function(){
+    var pikeUL = document.getElementById('pike');
+    for(var i = 0; i < hrPd.length; i++){
+     var liEl = document.createElement('li');
+     liEl.textContent = hrPd[i] + ':' + pike.cookiesPrHr[i] + ' cookies per hour';
+     pikeUL.appendChild(liEl);
+    }
+    var total = document.getElementById('pike');
+    var liEl = document.createElement('li');
+    liEl.textContent = 'total- ' + pike.cookiesPerDay(total);
+    total.appendChild(liEl);
 
   }
+};
 
-}
-    
+pike.custPh();
+pike.cookiesPerH();
+pike.render();
